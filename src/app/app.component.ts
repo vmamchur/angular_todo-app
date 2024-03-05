3165ee85fb534b18b8a65b1e04ab62470effa2e0
vmamchur <vmchr344@gmail.com>
2024-03-05 10:02:24 +0200

    feat: add counter of todos, implement basic todo addition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
-const todos = [
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+const todos: Todo[] = [
   { id: 1, title: 'HTML + CSS', completed: true },
   { id: 2, title: 'JS', completed: true },
   { id: 3, title: 'Angular', completed: false },
@@ -10,12 +17,47 @@ const todos = [
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   isEditing = false;
   todos = todos;
+  todoForm = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [
+        Validators.required,
+      ]
+    })
+  })
+
+  handleTodoDelete(index: number) {
+    todos.splice(index, 1);
+  }
+
+  get title() {
+    return this.todoForm.get('title') as FormControl;
+  }
+
+  get activeTodos() {
+    return this.todos.filter(todo => !todo.completed);
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid) {
+      return;
+    }
+
+    const newTodo: Todo = {
+      id: Date.now(),
+      title: this.title.value,
+      completed: false,
+    };
+
+    this.todos.push(newTodo);
+    this.todoForm.reset();
+  }
 }
   
